816fb1be24b2110c83ed8566033c92b289dfbe59
Michal Kubín <kubin.michal@gmail.com>
2022-06-16 23:27:10 +0200

    Upraven soubor users.py - uživatelé převedeni do slovníku pro snazší práci.
    Upraven soubor textovy_analyzator.py - základní podmínka ověřující uživatelské přístupové údaje

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -9,13 +9,24 @@ import task_template as tt
 import users
 
 # Vyžádá si od uživatele přihlašovací jméno a heslo
-# login =
+
+login = input("Please insert your login: ")
 
 # zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů,
+if str(login) in users.logins:
+    password = input("Please insert your password: ")
+
+    if str(password) in users.users[str(login)]:
+        print("Yes")
+    else:
+        print("Login details do not match. Terminating the program..")
+
 
 # pokud je registrovaný, pozdrav jej a umožni mu analyzovat texty,
 
 # pokud není registrovaný, upozorni jej a ukonči program.
+else:
+    terminate_txt = "Unregistered user, terminating the program.."
+    print(terminate_txt)
 
-terminate_txt = "Unregistered user, terminating the program.."
 
--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+"""
 +------+-------------+
 | user |   password  |
 +------+-------------+
@@ -6,3 +7,14 @@
 | mike | password123 |
 | liz  |   pass123   |
 +------+-------------+
+"""
+users = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123"
+}
+
+logins = users.keys()
+passes = users.values()
+log_pass = users.items()
