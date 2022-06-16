21d3cd03c65f01f46673b85f6fae49ccb37674dc
Michal Kubín <kubin.michal@gmail.com>
2022-06-16 22:08:22 +0200

    Kontaktní údaje + přidání souboru se zdrojovými texty

---
+++ textovy_analyzator.py
@@ -0,0 +1,8 @@
+"""
+textovy_analyzator.py: první projekt do Engeto Online Python Akademie
+author: Michal Kubín
+email: kubin.michal@gmail.com
+discord: Michal Kubín#0577
+"""
+
+import task_template as tt
