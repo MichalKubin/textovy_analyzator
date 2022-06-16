0f2373a4b5e32e191ed28c7354da9a1494db6cf2
Michal Kubín <kubin.michal@gmail.com>
2022-06-16 22:15:02 +0200

    Importován modul users, soubor pomocí komentů rozdělen do jednotlivých částí podle úkolů

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -6,3 +6,16 @@ discord: Michal Kubín#0577
 """
 
 import task_template as tt
+import users
+
+# Vyžádá si od uživatele přihlašovací jméno a heslo
+# login =
+
+# zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů,
+
+# pokud je registrovaný, pozdrav jej a umožni mu analyzovat texty,
+
+# pokud není registrovaný, upozorni jej a ukonči program.
+
+terminate_txt = "Unregistered user, terminating the program.."
+
